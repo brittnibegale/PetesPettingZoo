0e85875dee0e95563e7638c37872c4f41c94bb6c
brittnibegale <brittnibegale@gmail.com>
2017-10-23 08:35:51 -0500

    first commit of the project

---
+++ PetesPettingZoo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PetesPettingZoo.Startup))]
+namespace PetesPettingZoo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
