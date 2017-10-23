9f3c6752def69b13c816a8a02198538bb61c7b7d
ravenkostrzewa <ravekostrzewa@gmail.com>
2017-10-23 14:47:47 -0500

    added animals

--- PetesPettingZoo/Migrations/Configuration.cs
+++ PetesPettingZoo/Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 
 namespace PetesPettingZoo.Migrations
 {
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,18 +16,12 @@ namespace PetesPettingZoo.Migrations
 
         protected override void Seed(PetesPettingZoo.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Animals.AddOrUpdate(
+                a => a.AnimalName,
+                new Animal { AnimalName = "Lupe", AnimalType = "Alpaca", AtTheZoo = true },
+                new Animal { AnimalName = "Pancho", AnimalType = "Moon Bear", AtTheZoo = false },
+                new Animal { AnimalName = "Carlos", AnimalType = "Flamingo", AtTheZoo = true }
+           );
         }
     }
 }
