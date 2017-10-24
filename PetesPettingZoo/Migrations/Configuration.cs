fdb3695f2b5ddcbdd9caf1844c74e295b8736d62
ravenkostrzewa <ravekostrzewa@gmail.com>
2017-10-24 08:10:49 -0500

    added animals to the table

--- PetesPettingZoo/Migrations/Configuration.cs
+++ PetesPettingZoo/Migrations/Configuration.cs
@@ -20,7 +20,20 @@ namespace PetesPettingZoo.Migrations
                 a => a.AnimalName,
                 new Animal { AnimalName = "Lupe", AnimalType = "Alpaca", AtTheZoo = true },
                 new Animal { AnimalName = "Pancho", AnimalType = "Moon Bear", AtTheZoo = false },
-                new Animal { AnimalName = "Carlos", AnimalType = "Flamingo", AtTheZoo = true }
+                new Animal { AnimalName = "Carlos", AnimalType = "Flamingo", AtTheZoo = true },
+                new Animal { AnimalName = "Julio", AnimalType = "Emu", AtTheZoo = true },
+                new Animal { AnimalName = "Mateo", AnimalType = "Dolphin", AtTheZoo = true },
+                new Animal { AnimalName = "Rosa", AnimalType = "Meerkat", AtTheZoo = true },
+                new Animal { AnimalName = "Luis", AnimalType = "Electric Eel", AtTheZoo = false },
+                new Animal { AnimalName = "Efrain", AnimalType = "Kangaroo", AtTheZoo = true },
+                new Animal { AnimalName = "Dolce", AnimalType = "Penguin", AtTheZoo = true },
+                new Animal { AnimalName = "Edwardo", AnimalType = "Howler Monkey", AtTheZoo = false },
+                new Animal { AnimalName = "Mattias", AnimalType = "Tiger Shark", AtTheZoo = true },
+                new Animal { AnimalName = "Rolanda", AnimalType = "Cicada", AtTheZoo = true },
+                new Animal { AnimalName = "Rico", AnimalType = "Gazelle", AtTheZoo = true },
+                new Animal { AnimalName = "Maria", AnimalType = "Macaque", AtTheZoo = true },
+                new Animal { AnimalName = "Cristiano", AnimalType = "Iguana", AtTheZoo = false },
+                new Animal { AnimalName = "Juan", AnimalType = "Flying Squirrel", AtTheZoo = true }
            );
             context.Days.AddOrUpdate(
                 b => b.Day,
--- PetesPettingZoo/Models/Animal.cs
+++ PetesPettingZoo/Models/Animal.cs
@@ -10,8 +10,11 @@ namespace PetesPettingZoo.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "Name")]
         public string AnimalName { get; set; }
+        [Display(Name = "Type of Animal")]
         public string AnimalType { get; set; }
+        [Display(Name = "Is this Animal at the Petting Zoo?")]
         public bool AtTheZoo { get; set; }
     }
 }
