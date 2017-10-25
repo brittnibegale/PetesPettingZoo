aed1d3eb242c5618916653361d3745ea1ab71c8f
carlosv123 <vasquezcarlos1996@gmail.com>
2017-10-25 08:00:28 -0700

    created employee model

---
+++ PetesPettingZoo/Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PetesPettingZoo.Models
+{
+    public class Employee
+    {
+        [key]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Age")]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Sex")]
+        public string Sex { get; set; }
+
+
+
+
+
+
+    }
+}
